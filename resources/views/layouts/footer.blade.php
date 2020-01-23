2546b5c7c703c403f5a124e44394f508214eac24
jean philippe <bongojeanphilippe@gmail.com>
2020-01-23 16:02:07 +0100

    passer le nombre de ligne des tableau à 50

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -39,7 +39,7 @@
     <script>
         var table = $('#example').DataTable({
             "order": [],
-            "iDisplayLength": 10,
+            "iDisplayLength": 50,
             "language": {
             "decimal":        "",
             "emptyTable":     "Aucune donnée disponible dans le tableau",
@@ -64,7 +64,7 @@
         });
         var table1 = $('#example1').DataTable({
             "order": [],
-            "iDisplayLength": 10,
+            "iDisplayLength": 50,
 
             "language": {
             "decimal":        "",
@@ -91,7 +91,7 @@
         }); 
         // mandataire
         var table = $('#example3').DataTable({
-            // paging: false,
+            paging: false,
             "order": [],
             
 
