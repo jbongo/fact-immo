4563101e215c6debca12656c46f232dea7fb3cb0
jean philippe <bongojeanphilippe@gmail.com>
2020-02-11 11:48:54 +0100

    séparation entre dashbord amin et mandataire

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 
 
-
+@if(Auth::user()->role == "admin")
 <div class="row" >
       
     <div class="col-lg-8">
@@ -112,10 +112,158 @@
 
 </div>
 
+@else 
 
-@php
-    $tab = json_encode(config('stats.CA_N')[0]);
-@endphp
+
+
+<div class="row">
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card">
+            <div class="stat-widget-five">
+                <div class="stat-icon">
+                    <i class="ti-home bg-primary"></i>
+                </div>
+                <div class="stat-content">
+                    <div class="color-primary">Affaires </div>
+                    <div class="stat-text">27</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card">
+            <div class="stat-widget-five">
+                <div class="stat-icon">
+                    <i class="ti-money bg-success"></i>
+                </div>
+                <div class="stat-content">
+                    <div class=" color-success">Chiffre d'affaires</div>
+                    <div class="stat-text">5000</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card">
+            <div class="stat-widget-five">
+                <div class="stat-icon">
+                    <i class="ti-user bg-warning"></i>
+                </div>
+                <div class="stat-content">
+                    <div class=" color-primary">Mandataire</div>
+                    <div class="stat-text">200</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-6 col-sm-6">
+        <div class="card">
+            <div class="stat-widget-five">
+                <div class="stat-icon bg-danger">
+                    <i class="ti-sharethis"></i>
+                </div>
+                <div class="stat-content">
+                    <div class="color-primary">Partage</div>
+                    <div class="stat-text">6</div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- 
+<div class="row">
+    <div class="col-md-3">
+        <div class="card">
+            <div class="media">
+                <div class="media-left meida media-middle">
+                    <span><i class="ti-target f-s-22 color-primary border-primary round-widget"></i></span>
+                </div>
+                <div class="media-body media-text-right">
+                    <h4>278</h4>
+                    <h6>Aquéreurs potentiels</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="media">
+                <div class="media-left meida media-middle">
+                    <span><i class="ti-target f-s-22 color-warning border-warning round-widget"></i></span>
+                </div>
+                <div class="media-body media-text-right">
+                    <h4>45</h4>
+                    <h6>Aquéreurs ayant acheté un bien</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="media">
+                <div class="media-left meida media-middle">
+                    <span><i class="ti-target f-s-22 color-success border-success round-widget"></i></span>
+                </div>
+                <div class="media-body media-text-right">
+                    <h4>458</h4>
+                    <h6>Aquéreurs cherchants une maison</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-3">
+        <div class="card">
+            <div class="media">
+                <div class="media-left meida media-middle">
+                    <span><i class="ti-target f-s-22 border-danger color-danger round-widget"></i></span>
+                </div>
+                <div class="media-body media-text-right">
+                    <h4>895</h4>
+                    <h6>Aquéreurs cherchants un appartement</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> --}}
+
+
+<div class="content-wrap">
+        <div class="main">
+            <div class="container-fluid">
+       
+                <!-- /# row -->
+                <div id="main-content">
+                    <div class="row">
+                        <!-- column -->
+                        
+                        <!-- column -->
+                      
+                        <!-- column -->
+                    
+                        <div class="col-lg-12">
+                            <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Chiffre d'affaires mensuel 2019</h4>
+                                    <div id="morris-bar-chart"></div>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- column -->
+                        <!-- column -->
+                       
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="footer">
+                                <p>This dashboard was generated on <span id="date-time"></span> <a href="#" class="page-refresh">Refresh Dashboard</a></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endif
 
 @stop
 @section('js-content')
