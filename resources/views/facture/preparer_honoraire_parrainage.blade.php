5564569ea346121f890cf8e7adce81820011db73
jean philippe <bongojeanphilippe@gmail.com>
2020-07-15 17:54:54 +0200

    modif hono parrainnage

--- resources/views/facture/preparer_honoraire_parrainage.blade.php
+++ resources/views/facture/preparer_honoraire_parrainage.blade.php
@@ -65,12 +65,31 @@
 
          </div>
                <hr>
-    @if(Auth()->user()->role == "admin" && $facture != null )
-
-        {{-- @if(!in_array($facture->statut, ["valide","en attente de validation"]) ) --}}
-
-            <a href="{{route('facture.recalculer_honoraire',Crypt::encrypt($facture->id))}}" class="btn btn-danger btn-flat btn-addon btn-lg m-b-10 m-l-5 submit" id="ajouter"><i class="ti-reload"></i>Recalculer</a>
-        {{-- @endif --}}
+               <div class="row">
+                <div class="col-md-4"> 
+                    @if(Auth()->user()->role == "admin" && $facture != null )
+                        <a href="{{route('facture.recalculer_honoraire',Crypt::encrypt($facture->id))}}" class="btn btn-danger btn-flat btn-addon btn-lg m-b-10 m-l-5 submit" id="ajouter"><i class="ti-reload"></i>Recalculer</a>
+                    @endif
+                </div>
+                <div class="col-md-4">
+                    @if($facture != null)
+                     @if($facture->statut == "valide" && $facture->url != null)
+                         <a class="color-info" title="Télécharger la facture d'honoraire "  href="{{route('facture.telecharger_pdf_facture', Crypt::encrypt($facture->id))}}"  class="  m-b-10 m-l-5 " id="ajouter">Télécharger Fac: {{$facture->numero}} <i class="ti-download"></i> </a>
+                     @elseif($facture->statut == "en attente de validation")
+                         <label class="color-danger" ><strong> Facture en attente de validation </strong> &nbsp;  </label> 
+                         @if(Auth()->user()->role == "admin")
+                            <a href="{{route('facture.valider_honoraire', [1,Crypt::encrypt($facture->id)] )}}"  class="btn btn-success btn-flat btn-addon  m-b-10 m-l-5 valider" id="valider"><i class="ti-check"></i>Valider</a>
+                            <a href="{{route('facture.valider_honoraire', [0,Crypt::encrypt($facture->id)] )}}"  class="btn btn-danger btn-flat btn-addon  m-b-10 m-l-5 refuser" id="refuser"><i class="ti-close"></i>Réfuser</a>
+                        @endif
+                    @elseif($facture->statut == "refuse")
+                         <label class="color-danger" ><strong> Facture réfusée </strong> </label>
+                     @else
+                     <label class="color-danger" ><strong> Facture non Ajoutée </strong> </label>
+                     @endif 
+                @endif 
+                </div>
+            </div>
+                
     @endif
 
 <table style="height: 59px; width: 311px;">
