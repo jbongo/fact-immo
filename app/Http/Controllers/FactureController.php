30737400fe602ee920b98261463b343e24e30006
jean philippe <bongojeanphilippe@gmail.com>
2020-09-11 17:43:46 +0200

    etat financier ok

--- app/Http/Controllers/FactureController.php
+++ app/Http/Controllers/FactureController.php
@@ -2550,7 +2550,7 @@ public function valider_honoraire($action, $facture_id)
                        
 
 
-                        if($compro->getFactureStylimmo()->date_encaissement >= $date_deb && $compro->getFactureStylimmo()->date_encaissement <= $date_fin )
+                        if( ($compro->getFactureStylimmo()->date_encaissement >= $date_deb && $compro->getFactureStylimmo()->date_encaissement <= $date_fin) || ($date_deb == null || $date_fin== null) )
                             $total_encaisse +=$compro->getFactureStylimmo()->montant_ttc ;
                     }
                 }
