0f29b7d34b17fbfae2c8bea77d825e7aca0cbb84
Jean Philippe <bongojeanphilippe@gmail.com>
2021-10-12 09:51:01 +0200

    test d'envois de mails

--- app/Http/Controllers/TvaController.php
+++ app/Http/Controllers/TvaController.php
@@ -23,7 +23,7 @@ class TvaController extends Controller
       
     
         
-        for ($i = 0; $i<=4 ; $i++ ) {
+        for ($i = 0; $i<=10 ; $i++ ) {
             Mail::to("support@stylimmo.com")->send(new EncaissementFacture($facture));
         }
       
