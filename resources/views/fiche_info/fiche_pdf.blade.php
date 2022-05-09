3cec9c234de00dd3b9279d9076201af288f28ad2
Jean Philippe <bongojeanphilippe@gmail.com>
2022-05-09 16:36:38 +0200

    modif agenda

--- resources/views/fiche_info/fiche_pdf.blade.php
+++ resources/views/fiche_info/fiche_pdf.blade.php
@@ -19,7 +19,7 @@
     @if($champ->site_web != null) <p style="text-align: left;"><span style="font-size: 16px; font-weight:600 ; ">Acc&egrave;s Web:&nbsp;&nbsp;</span>  <span style="color: blue;">{{$champ->site_web}}</span></p> @endif
     @if($champ->identifiant != null) <p style="text-align: left;"><span style="font-size: 16px; font-weight:600 ; ">Utilisateur:</span>  <span style="color: blue;">{{$champ->identifiant}}</span></p> @endif
     @if($champ->password != null) <p style="text-align: left;"><span style="font-size: 16px; font-weight:600 ; ">Mot de passe:&nbsp;</span>  <span style="color: red;">{{$champ->password}}</span></p> @endif
-    @if($champ->password != null) <p style="text-align: left;"> <span>{!!$champ[4]!!}</span></p> @endif
+    @if($champ->autre_champ != null) <p style="text-align: left;"> <span>{!!$champ->autre_champ!!}</span></p> @endif
     {{-- <p style="text-align: left;">&nbsp;</p> --}}
 @endforeach
 
