b29b7cffe93dd90fd78f47d07c346041d375ecfd
Jean Philippe <bongojeanphilippe@gmail.com>
2022-08-03 10:06:16 +0200

    debug export

--- app/Http/Controllers/ExportwinficController.php
+++ app/Http/Controllers/ExportwinficController.php
@@ -39,11 +39,10 @@ class ExportwinficController extends Controller
             $date_fin = date("Y-m-d");        
         }     
         
-        // $factureStylimmos = Facture::whereIn('type',['stylimmo','avoir','pack_pub','carte_visite','communication','autre','forfait_entree','cci'])->whereBetween('date_facture',[$date_deb,$date_fin])->where('user_id','<>',77)->orderBy('numero','asc')->get();  
-        $factureStylimmos = Facture::whereIn('type',['stylimmo','avoir','pack_pub','carte_visite','communication','autre','forfait_entree','cci'])->whereBetween('date_facture',[$date_deb,$date_fin])->orderBy('numero','asc')->get();  
+        $factureStylimmos = Facture::whereIn('type',['stylimmo','avoir','pack_pub','carte_visite','communication','autre','forfait_entree','cci'])->whereBetween('date_facture',[$date_deb,$date_fin])->where('user_id','<>',77)->orderBy('numero','asc')->get();  
         $montant_credit_debit = Facture::whereIn('type',['stylimmo','avoir','pack_pub','carte_visite','communication','autre','forfait_entree','cci'])->whereBetween('date_facture',[$date_deb,$date_fin])->where('user_id','<>',77)->sum('montant_ttc');
         
-    //   dd($factureStylimmos);
+      
         $this->generer_pdf_facture($date_deb, $date_fin);
        
         return view ('winfic.index',compact(['factureStylimmos','date_deb','date_fin','montant_credit_debit']));
