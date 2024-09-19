2d96c85c51713c909956ada29adc87bbc693905b
Jean Philippe <bongojeanphilippe@gmail.com>
2024-09-19 09:50:53 +0200

    descativation csrf  pour debogage

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,6 +23,7 @@ class Kernel extends HttpKernel
 
     /**
      * The application's route middleware groups.
+     
      *
      * @var array
      */
@@ -33,7 +34,7 @@ class Kernel extends HttpKernel
             \Illuminate\Session\Middleware\StartSession::class,
             // \Illuminate\Session\Middleware\AuthenticateSession::class,
             \Illuminate\View\Middleware\ShareErrorsFromSession::class,
-            \App\Http\Middleware\VerifyCsrfToken::class,
+            // \App\Http\Middleware\VerifyCsrfToken::class,
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             // \App\Http\Middleware\Admin::class,
         ],
