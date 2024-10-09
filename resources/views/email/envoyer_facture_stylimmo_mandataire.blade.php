19247cb5a354406bbb010b48afad8c34a53d9cce
Jean Philippe <bongojeanphilippe@gmail.com>
2024-10-09 15:32:40 +0200

    modif mail facture stylimmo bug numéro cci

--- resources/views/email/envoyer_facture_stylimmo_mandataire.blade.php
+++ resources/views/email/envoyer_facture_stylimmo_mandataire.blade.php
@@ -6,7 +6,7 @@
 STYL'IMMO 
 @elseif($facture->type == "cci")
 {{-- Attestation de Collaborateur (CCI)  --}}
-Après vérification, une erreur a été constatée dans le numéro de la facture CCi précédemment envoyée. <br>
+Après vérification, une erreur a été constatée dans le numéro de la facture CCI précédemment envoyée. <br>
 
 Veuillez trouver ci-joint la version corrigée de la facture.<br>
 Nous vous prions de bien vouloir ne pas prendre en compte la version initiale.<br>
